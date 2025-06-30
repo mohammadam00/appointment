5b72284fa145f939aea515994ccd9685fb51722f
Mohamad Akbari <mohamadma8878@gmail.com>
2025-07-01 00:05:42 +0330

    a

---
+++ appointment_doctor_project/local_settings.py
@@ -0,0 +1,9 @@
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+DB_NAME = "postgres"
+DB_PORT = "5432"
+DB_USER = "postgres"
+DB_PASSWORD = "ma54859130"
+DB_HOST = 'localhost'
--- appointment_doctor_project/settings.py
+++ appointment_doctor_project/settings.py
@@ -152,10 +152,4 @@ SIMPLE_JWT = {
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
 
-DB_NAME = "postgres"
-DB_PORT = "5432"
-DB_USER = "postgres"
-DB_PASSWORD = "ma54859130"
-DB_HOST = 'localhost'
